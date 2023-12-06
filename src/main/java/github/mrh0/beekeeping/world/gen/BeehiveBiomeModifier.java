e796b25b635ad8f9e110cf401b00f94d1f5c324f
unilock <unilock@fennet.rentals>
2023-12-06 17:33:44 -0500

    Exclude broken code for now
    Signed-off-by: unilock <unilock@fennet.rentals>

--- src/main/java/github/mrh0/beekeeping/world/gen/BeehiveBiomeModifier.java
+++ src/main/java/github/mrh0/beekeeping/world/gen/BeehiveBiomeModifier.java
@@ -1,18 +1,14 @@
 package github.mrh0.beekeeping.world.gen;
 
 import github.mrh0.beekeeping.Beekeeping;
-import github.mrh0.beekeeping.bee.Specie;
-import github.mrh0.beekeeping.bee.SpeciesRegistry;
-import github.mrh0.beekeeping.world.feature.BeekeepingPlacedFeatures;
 import net.fabricmc.fabric.api.biome.v1.BiomeModification;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
-import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
-import net.minecraft.world.level.levelgen.GenerationStep.Decoration;
 
 public class BeehiveBiomeModifier {
 	private static final BiomeModification BIOME_MODIFICATION = BiomeModifications.create(Beekeeping.get("biome_modification"));
 
     public static void modify() {
+		/*
         // TODO: this is probably not ideal and we should probably move all the beehive placement into data jsons akin to:
         // https://github.com/MinecraftForge/MinecraftForge/blob/1.19.x/src/generated_test/resources/data/biome_modifiers_test/forge/biome_modifier/add_basalt.json
         for (Specie species : SpeciesRegistry.instance.getAll()) {
@@ -26,5 +22,6 @@ public class BeehiveBiomeModifier {
 				modificationCtx -> modificationCtx.getGenerationSettings().addBuiltInFeature(Decoration.VEGETAL_DECORATION, BeekeepingPlacedFeatures.getPlacedFeatures(species.beehive, species.beehive.rarity, species.beehive.modifier, species.beehive.feature).value())
 			);
         }
+		 */
     }
 }
