de40f8c5fe375dead9b921150d1aa50f08bd5f4c
unilock <unilock@fennet.rentals>
2023-12-07 14:13:49 -0500

    Fix BiomeModifications - beehives now generate naturally
    Signed-off-by: unilock <unilock@fennet.rentals>

--- README.md
+++ README.md
@@ -3,6 +3,5 @@ Minecraft Forge mod by MRH0
 Fabric port by unilock
 
 TODO:
-- BiomeModifications
 - DataGenerator
 - TogglePacket
--- src/main/java/github/mrh0/beekeeping/world/gen/BeehiveBiomeModifier.java
+++ src/main/java/github/mrh0/beekeeping/world/gen/BeehiveBiomeModifier.java
@@ -1,27 +1,58 @@
 package github.mrh0.beekeeping.world.gen;
 
 import github.mrh0.beekeeping.Beekeeping;
+import github.mrh0.beekeeping.bee.Specie;
+import github.mrh0.beekeeping.bee.SpeciesRegistry;
 import net.fabricmc.fabric.api.biome.v1.BiomeModification;
 import net.fabricmc.fabric.api.biome.v1.BiomeModifications;
+import net.fabricmc.fabric.api.biome.v1.ModificationPhase;
+import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.feature.Feature;
+import net.minecraft.world.level.levelgen.feature.configurations.RandomPatchConfiguration;
+import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConfiguration;
+import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.placement.BiomeFilter;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
+import net.minecraft.world.level.levelgen.placement.RarityFilter;
 
 public class BeehiveBiomeModifier {
 	private static final BiomeModification BIOME_MODIFICATION = BiomeModifications.create(Beekeeping.get("biome_modification"));
 
     public static void modify() {
-		/*
         // TODO: this is probably not ideal and we should probably move all the beehive placement into data jsons akin to:
         // https://github.com/MinecraftForge/MinecraftForge/blob/1.19.x/src/generated_test/resources/data/biome_modifiers_test/forge/biome_modifier/add_basalt.json
         for (Specie species : SpeciesRegistry.instance.getAll()) {
-            if (!species.hasBeehive()) {
-                continue;
-            }
+            if (species.hasBeehive()) {
+				var configuredFeature = FeatureUtils.register(
+					Beekeeping.get(species.beehive.getName() + "_configured").toString(),
+					species.beehive.feature,
+					new RandomPatchConfiguration(
+						species.beehive.tries,
+						16,
+						2,
+						PlacementUtils.onlyWhenEmpty(
+							Feature.SIMPLE_BLOCK,
+							new SimpleBlockConfiguration(
+								BlockStateProvider.simple(species.beehive.block.get())
+							)
+						)
+					)
+				);
 
-			BIOME_MODIFICATION.add(
-				ModificationPhase.ADDITIONS,
-				selectionCtx -> species.beehive.acceptsBiome(selectionCtx.getBiomeRegistryEntry()),
-				modificationCtx -> modificationCtx.getGenerationSettings().addBuiltInFeature(Decoration.VEGETAL_DECORATION, BeekeepingPlacedFeatures.getPlacedFeatures(species.beehive, species.beehive.rarity, species.beehive.modifier, species.beehive.feature).value())
-			);
+				var placedFeature = PlacementUtils.register(
+					Beekeeping.get(species.beehive.getName() + "_placed").toString(),
+					configuredFeature,
+					RarityFilter.onAverageOnceEvery(species.beehive.rarity), InSquarePlacement.spread(), species.beehive.modifier, BiomeFilter.biome()
+				);
+
+				BIOME_MODIFICATION.add(
+					ModificationPhase.ADDITIONS,
+					selectionCtx -> species.beehive.acceptsBiome(selectionCtx.getBiomeRegistryEntry()),
+					modificationCtx -> modificationCtx.getGenerationSettings().addBuiltInFeature(GenerationStep.Decoration.VEGETAL_DECORATION, placedFeature.value())
+				);
+			}
         }
-		 */
     }
 }
